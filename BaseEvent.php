d1a09baa407ae6b77d1b1b34df194d032061e90f
basic-app <dev@basic-app.com>
2020-10-02 22:19:54 +0300

    n/a

--- BaseEvent.php
+++ BaseEvent.php
@@ -8,15 +8,25 @@ namespace BasicApp\Event;
 
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
+use Psr\Log\LoggerInterface;
 
 abstract class BaseEvent implements EventInterface
 {
 
-    use LoggerAwareTrait;
+    use LoggerAwareTrait {
+        setLogger as setLoggerTrait;
+    }    
 
     public function __construct()
     {
         $this->setLogger(new NullLogger);
+    }
+
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->setLoggerTrait($logger);
+
+        return $this;
     }
 
 }
